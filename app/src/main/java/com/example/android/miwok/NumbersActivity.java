34bf45c8358859cc4415239b249ef6f9aff62599
tr0lczyk <matt.olczyk@gmail.com>
2018-01-10 22:04:17 +0100

    added the intent to open Numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,4 +12,6 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
     }
+
+
 }
