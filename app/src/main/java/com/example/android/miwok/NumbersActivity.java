19c92fcb996b42cf9144e6e65b1c4523b64a71cc
tr0lczyk <matt.olczyk@gmail.com>
2018-01-11 23:29:24 +0100

    creating the new objects through the java NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -12,27 +16,22 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        String [] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
-        Log.i("NumbersActivity","The word you are looking for is : " + words[0]);
-        Log.i("NumbersActivity","The word you are looking for is : " + words[1]);
-        Log.i("NumbersActivity","The word you are looking for is : " + words[2]);
-        Log.i("NumbersActivity","The word you are looking for is : " + words[3]);
-        Log.i("NumbersActivity","The word you are looking for is : " + words[4]);
-        Log.i("NumbersActivity","The word you are looking for is : " + words[5]);
-        Log.i("NumbersActivity","The word you are looking for is : " + words[6]);
-        Log.i("NumbersActivity","The word you are looking for is : " + words[7]);
-        Log.i("NumbersActivity","The word you are looking for is : " + words[8]);
-        Log.i("NumbersActivity","The word you are looking for is : " + words[9]);
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView =(LinearLayout) findViewById(R.id.rootView);
+        TextView wordView = new TextView(this);
+        wordView.setText(words.get(0));
+        rootView.addView(wordView);
     }
 
 
