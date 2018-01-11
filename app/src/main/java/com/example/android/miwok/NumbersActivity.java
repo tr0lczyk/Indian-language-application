6b66497eda75b3449019bb82c455b63bef53869d
tr0lczyk <matt.olczyk@gmail.com>
2018-01-11 22:15:22 +0100

    updated numbers array

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class NumbersActivity extends AppCompatActivity {
@@ -22,6 +23,16 @@ public class NumbersActivity extends AppCompatActivity {
         words[7] = "eight";
         words[8] = "nine";
         words[9] = "ten";
+        Log.i("NumbersActivity","The word you are looking for is : " + words[0]);
+        Log.i("NumbersActivity","The word you are looking for is : " + words[1]);
+        Log.i("NumbersActivity","The word you are looking for is : " + words[2]);
+        Log.i("NumbersActivity","The word you are looking for is : " + words[3]);
+        Log.i("NumbersActivity","The word you are looking for is : " + words[4]);
+        Log.i("NumbersActivity","The word you are looking for is : " + words[5]);
+        Log.i("NumbersActivity","The word you are looking for is : " + words[6]);
+        Log.i("NumbersActivity","The word you are looking for is : " + words[7]);
+        Log.i("NumbersActivity","The word you are looking for is : " + words[8]);
+        Log.i("NumbersActivity","The word you are looking for is : " + words[9]);
     }
 
 
